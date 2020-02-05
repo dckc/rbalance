e7418343100d4a83bca7d9dd3f5e09f0d6471b98
Dan Connolly <dconnolly@kumc.edu>
2020-02-05 17:27:09 -0600

    call reportAdjustments from Main

--- src/main/scala/coop/rchain/rbalanceweb/Main.scala
+++ src/main/scala/coop/rchain/rbalanceweb/Main.scala
@@ -2,10 +2,12 @@ package coop.rchain.rbalanceweb
 
 import cats.effect._
 import cats.implicits._
+import coop.rchain.rbalance.txns.RHOCTxnGraphClosure.{BarcelonaClique, PithiaClique}
 
 object Main extends IOApp {
   def run(args: List[String]) = {
-    //RHOCTxnClosure.close( new InitialRHOCTxn( "0xEb148735F7e75B41AAF344CDa706b8F95d5E39d4" ) )
+    PithiaClique.reportAdjustments()
+    BarcelonaClique.reportAdjustments()
     IO{ ExitCode.Success }
   }
 }
